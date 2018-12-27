b8ba6afc91f1b74e8903810aaa38ee3cd677fab8
vgrillot <grivin@gmail.com>
2018-12-27 13:43:40 +0100

    fix driver pulse function (V5)

--- rasppinball_platform/driver.py
+++ rasppinball_platform/driver.py
@@ -33,25 +33,15 @@ class RASPDriver(DriverPlatformInterface):
         """Return the name of the board of this driver."""
         pass
 
-    def pulse(self, milliseconds):
+    def pulse(self, pulse_settings: PulseSettings):
         """Pulse a driver.
 
         Pulse this driver for a pre-determined amount of time, after which
         this driver is turned off automatically. Note that on most platforms,
         pulse times are a max of 255ms. (Beyond that MPF will send separate
         enable() and disable() commands.
-
-        Args:
-            milliseconds: The number of ms to pulse this driver for. You should
-                raise a ValueError if the value is out of range for your
-                platform.
-
-        Returns:
-            A integer of the actual time this driver is going to be pulsed for.
-            MPF uses this for timing in certain situations to make sure too
-            many drivers aren't activated at once.
-
         """
-        self.log.info("RASPDriver.Pulse(%s, %d ms)" % (self.number, milliseconds))
-        self.platform.communicator.driver_pulse(self.number, milliseconds)
-        return milliseconds
+        ms = pulse_settings.duration
+        self.log.info("RASPDriver.Pulse(%s, %d ms)" % (self.number, ms))
+        self.platform.communicator.driver_pulse(self.number, ms)
+        return ms
